e789d2c9935a79524d283d0b97447da6475e5a81
uzaib1356 <101474221+uzaib1356@users.noreply.github.com>
2022-03-13 05:31:43 -0700

    Add files via upload

---
+++ class4.dart
@@ -0,0 +1,41 @@
+void main() {
+  //==================MAP=================
+  // Map<String, String> data = {'Name': 'John Doe', 'Occupation': 'gardner'};
+  // print(data);
+  // print(data.keys);
+  // print(data.values);
+  //============================
+  //  Map<String, dynamic> data = {'DOB': 19/9/1999, 'CNIC': 42201-9586-78};
+  // print(data);
+  // print(data.keys);
+  // print(data.values);
+  //===================================
+  // Map student_data = {
+  //   'name': 'uzaib',
+  //   'p_no': "03111-029416",
+  //   'per_in_XII': 78.85
+  // };
+  //print(student_data);
+  //print(student_data["name"]);
+  //List data = [1, 12.45, "uzaib", student_data];
+  //print(data[3]);
+  //print(data[3]["p_no"]);
+  //print("${student_data["name"]} phone:  ${student_data["p_no"].toString()}");
+  // var words = {
+  //   1: 'apple',
+  //   2: 'keela',
+  //   3: 'ball',
+  //   4: 'bat',
+  //   5: 'grape',
+  //   6: 'moon',
+  // };
+  // // words.remove(1);
+
+  // //words.removeWhere((key, value) => value.startsWith('k'));
+  // var fruit = "mango";
+  // var val = words.putIfAbsent(7,()=>fruit);
+  // print(words);
+
+//======================MERGR METHOD===================
+//======================LOOPS==========================
+}
